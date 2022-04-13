362d655070124d34cf6d44304d5e5a222a4099a5
mikeymikey08 <103610568+mikeymikey08@users.noreply.github.com>
2022-04-13 15:53:05 -0700

    Add files via upload

---
+++ htdocs/math1.js
@@ -0,0 +1,15 @@
+function multipleBy()
+{
+		num1 = document.getElementById("firstNumber").value;
+		num2 = document.getElementById("secondNumber").value;
+		document.getElementById("result".innerHTML = num1 * num2;
+}
+
+function divivdeBy()
+
+{
+		num1 = document.getElementById("firstNumber").value;
+		num2 = document.getElementById("secondNumber").value;
+		document.getelementById(result).innerHTMl = num1 / num2;
+		
+}
